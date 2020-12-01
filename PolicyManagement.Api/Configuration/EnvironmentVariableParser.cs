4cd7ac06b25c7a551e0b28d9c9dc537bd51422f9
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-12-01 15:25:09 +0000

    Add Policy Distribution on settings change

--- PolicyManagement.Api/Configuration/EnvironmentVariableParser.cs
+++ PolicyManagement.Api/Configuration/EnvironmentVariableParser.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using Glasswall.PolicyManagement.Common.Configuration;
 using Glasswall.PolicyManagement.Common.Configuration.Validation;
 using Glasswall.PolicyManagement.Common.Configuration.Validation.Errors;
+using Microsoft.Extensions.Configuration;
 
 namespace Glasswall.PolicyManagement.Business.Configuration
 {
     public class EnvironmentVariableParser : IConfigurationParser
     {
+        private readonly IConfiguration _configuration;
         private readonly IDictionary<string, IConfigurationItemValidator> _configurationBinders;
 
-        public EnvironmentVariableParser(IDictionary<string, IConfigurationItemValidator> configurationBinders)
+        public EnvironmentVariableParser(IConfiguration configuration, IDictionary<string, IConfigurationItemValidator> configurationBinders)
         {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _configurationBinders = configurationBinders ?? throw new ArgumentNullException(nameof(configurationBinders));
         }
 
@@ -23,7 +26,7 @@ namespace Glasswall.PolicyManagement.Business.Configuration
 
             foreach (var property in typeof(TConfiguration).GetProperties())
             {
-                var rawValue = Environment.GetEnvironmentVariable(property.Name);
+                var rawValue = _configuration[property.Name];
 
                 if (_configurationBinders[property.Name].TryParse(property.Name, rawValue, errors, out var parsed))
                 {
---
+++ PolicyManagement.Common/BackgroundServices/IPolicySynchronizer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Glasswall.PolicyManagement.Common.BackgroundServices
+{
+    public interface IPolicySynchronizer
+    {
+        Task SyncPoliciesAsync(CancellationToken cancellationToken);
+    }
+}
